4bada31fe17d2eca26a9c0b122840081368468a4
Eduardo Hartz <80062347+eduardohartz@users.noreply.github.com>
2025-08-09 09:31:43 -0700

    add qr code generation and scanning support

--- src/irsattend/scanner/barcode_generator.py
+++ src/irsattend/scanner/barcode_generator.py
@@ -1,17 +1,25 @@
 import os
 from barcode import Code39
 from barcode.writer import ImageWriter
+import segno
+
 from .. import config
 
-def generate_barcode_image(data: str, filename: str) -> str:
+def generate_barcode_image(data: str, filename: str, format: str) -> str:
     """
-    Generates a Code 39 barcode image and saves it to a file.
+    Generates a Code 39 barcode or a QR Code image and saves it to a file.
     Data is student ID, filename should also be unique, probably "student ID.png"
     """
     if not os.path.exists(config.BAR_CODE_DIR):
         os.makedirs(config.BAR_CODE_DIR)
-
-    code39 = Code39(data, writer=ImageWriter(), add_checksum=False)
-    filepath = os.path.join(config.BAR_CODE_DIR, filename.replace('.png', ''))
-    code39.save(filepath, options={'write_text': False})  # No text below code
-    return filepath + '.png'
+        
+    if format == 'code39':
+        code39 = Code39(data, writer=ImageWriter(), add_checksum=False)
+        filepath = os.path.join(config.BAR_CODE_DIR, filename.replace('.png', ''))
+        code39.save(filepath, options={'write_text': False})  # No text below code
+        return filepath + '.png'
+    elif format == 'QRCode':
+        qrcode = segno.make_qr(data, error='H')
+        filepath = os.path.join(config.BAR_CODE_DIR, filename)
+        qrcode.save(filepath, border=3)
+        return filepath
--- src/irsattend/scanner/barcode_reader.py
+++ src/irsattend/scanner/barcode_reader.py
@@ -4,5 +4,5 @@ import numpy as np
 
 def read_barcodes(frame: np.ndarray) -> List[zxingcpp.Result]:
     """Reads Code 39 barcodes from a frame."""
-    results = zxingcpp.read_barcodes(frame, formats=zxingcpp.BarcodeFormat.Code39) # Read only Code 39 barcodes
+    results = zxingcpp.read_barcodes(frame, formats=zxingcpp.BarcodeFormat.Code39 | zxingcpp.BarcodeFormat.QRCode) # Read only Code 39 and QR Codes
     return results
